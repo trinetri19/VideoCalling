7207cbfc4cb4798648fbb1a4794addc6045731b5
trinetri19 <rajagraharitrinetri@gmail.com>
2025-09-04 01:20:21 +0530

    Backend url fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Landing from './pages/landing'
+import Landing from './pages/Landing'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './pages/templates/Navbar';
 import Register from './pages/Authentication/Register/Register';
